3275fb86a586054f5cd22780460da622188fc615
Margo <PANTARA_95@mail.ru>
2022-04-08 18:08:14 +0300

    Fill the form

--- src/test/java/Tests/StudentFormTests.java
+++ src/test/java/Tests/StudentFormTests.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
+import java.io.File;
+
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class StudentFormTests {
@@ -13,6 +16,8 @@ public class StudentFormTests {
     static void setUp() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.holdBrowserOpen = true;
+
+
 
     }
 
@@ -23,11 +28,14 @@ public class StudentFormTests {
         String useremail = "Jane@dow.com";
         String usermobile = "70800000000";
         String dateofbirth = "07 Mar 1990";
-        String subject = "Marketing technologies";
+        String subject = "Maths";
+        String currenraddress = "Somestreet 123";
 
         open("/automation-practice-form");
 
         //Fill the form
+
+        executeJavaScript("$('footer').remove()");
         $("#firstName").setValue(firstname);
         $("#lastName").setValue(lastname);
         $("#userEmail").setValue(useremail);
@@ -36,9 +44,25 @@ public class StudentFormTests {
         $("#dateOfBirthInput").sendKeys((Keys.CONTROL + "a"));
         $("#dateOfBirthInput").sendKeys((Keys.SPACE));
         $("#dateOfBirthInput").setValue(dateofbirth).pressEnter();
-        $("#subjectsInput").setValue(subject);
+        $("#subjectsInput").setValue(subject).pressEnter();
         $("#hobbies-checkbox-2").closest("div").click();
         $("#hobbies-checkbox-3").closest("div").click();
+        $("#currentAddress").setValue(currenraddress);
+        $("#state").click();
+        $(byText("Uttar Pradesh")).click();
+        $("#city").click();
+        $(byText("Agra")).click();
+        $("#close-fixedban").click();
+        $("#submit").scrollIntoView("false").click();
+
+        //Asserts
+
+
+
+
+
+
+
 
 
 
